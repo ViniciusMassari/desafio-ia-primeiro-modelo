d5e24daaa8fa65145dc0ac40b6cdf8aade43b4e7
Vinícius Massari <stonefields11@hotmail.com>
2024-08-15 16:43:40 -0300

    :tada: initial commit

---
+++ vendas.py
@@ -0,0 +1,4 @@
+dados_vendas = {
+    'mes': ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    'vendas': [2000, 2200, 2300, 2500, 2600, 2700, 2800, 2900, 3000, 3100, 3200, 3300]
+}
